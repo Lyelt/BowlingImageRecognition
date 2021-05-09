019c83e85c1bebf2d235d8c89a966e788711b4b4
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-05-09 13:45:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Drawing;
+
+namespace BowlingImageExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = args.FirstOrDefault();
+            if (!string.IsNullOrWhiteSpace(filePath) && File.Exists(filePath))
+            {
+                using var bmp = new Bitmap(filePath);
+                var text = ImageExtraction.Test.ExtractText(bmp);
+                Console.WriteLine($"Extracted: {text}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
